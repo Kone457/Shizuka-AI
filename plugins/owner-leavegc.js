ed95e034b64766a611f7c985a73652ef310195af
Carlos <supernumerovirtual@gmail.com>
2025-08-05 14:04:19 -0400

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,16 +1,21 @@
 let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-let chat = global.db.data.chats[m.chat]
-chat.welcome = false
-await conn.reply(id, `${emoji} Adios a todos, el Bot se despide! (≧ω≦)ゞ`) 
-await conn.groupLeave(id)
-try {  
-chat.welcome = true
-} catch (e) {
-await m.reply(`${fg}`) 
-return console.log(e)
-}}
-handler.command = ['salir','leavegc','salirdelgrupo','leave']
+  let id = text ? text : m.chat  
+  let chat = global.db.data.chats[m.chat]
+  
+  chat.welcome = false
+
+  await conn.reply(id, `🌸 Shizuka suspira profundo y se despide con una reverencia…\n「Gracias por permitirme ser parte de su historia.」\n¡Hasta pronto, mis queridos! (≧◡≦)ゞ`) 
+  await conn.groupLeave(id)
+
+  try {  
+    chat.welcome = true
+  } catch (e) {
+    await m.reply(`⚠️ Shizuka ha tropezado en la niebla... pero se levantará con más elegancia 💫`) 
+    return console.log(e)
+  }
+}
+
+handler.command = ['salir', 'leavegc', 'salirdelgrupo', 'leave']
 handler.group = true
 handler.rowner = true
 
