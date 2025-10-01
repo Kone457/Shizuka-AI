5125c3f5eaf3cc0b103a2325c0c3e0f8419fc761
Carlos <supernumerovirtual@gmail.com>
2025-09-30 23:02:50 -0400

    Actualizar fun-a.js

--- plugins/fun-a.js
+++ plugins/fun-a.js
@@ -4,16 +4,16 @@ const handler = async (m, { conn }) => {
   if (texto !== 'A') return;
 
   const imageUrl = 'https://qu.ax/ZdOdf.jpg';
-  
+
   await conn.sendMessage(m.chat, {
-    image: { url: imageUrl },
-    { quoted: m });
+    image: { url: imageUrl }
+  }, { quoted: m });
 };
 
 handler.customPrefix = /^A$/i; 
 handler.command = new RegExp(); 
-handler.group = false;          
-handler.admin = false;          
+handler.group = false;
+handler.admin = false;
 handler.botAdmin = false;
 handler.tags = ['imagen'];
 handler.help = ['A'];
