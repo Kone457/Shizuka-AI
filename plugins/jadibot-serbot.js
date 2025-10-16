8046f17628ca1c9c404fbd04130e7c12e0cf6e98
Carlos <supernumerovirtual@gmail.com>
2025-10-15 20:25:31 -0400

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -68,7 +68,8 @@ global.db.data.users[m.sender].Subs = new Date * 1
 } 
 handler.help = ['qr', 'code']
 handler.tags = ['serbot']
-handler.command = ['qr', 'code']
+handler.command = ['qr......
+', 'code.........']
 export default handler 
 
 export async function yukiJadiBot(options) {
