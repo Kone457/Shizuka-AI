70764391a006c1b3cb911a8eae2b1f00ece7a667
Carlos <supernumerovirtual@gmail.com>
2025-10-20 20:59:11 -0400

    Actualizar info-userid.js

--- plugins/info-userid.js
+++ plugins/info-userid.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, isOwner }) => {
 let USER_ID = m.user.lid 
 conn.fakeReply(m.chat, USER_ID, '0@s.whatsapp.net', `👇 AQUI ESTA TU NUMERO OCULTO "LID" 👇`, 'status@broadcast')
-//m.reply(USER_ID)
+m.reply(USER_ID)
 }
 handler.help = ['mylid'];
 handler.tags = ['tools'];
