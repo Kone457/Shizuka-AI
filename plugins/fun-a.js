6fc9cc11548e46e8318184ed04d281e3d4a84ad9
Carlos <supernumerovirtual@gmail.com>
2025-10-21 20:59:26 -0400

    Actualizar fun-a.js

--- plugins/fun-a.js
+++ plugins/fun-a.js
@@ -3,15 +3,20 @@ const handler = async (m, { conn }) => {
 
   if (texto !== 'A') return;
 
-  const imageUrl = ['https://raw.githubusercontent.com/Kone457/Nexus/refs/heads/main/Ruta-(1)/A.png','https://qu.ax/fYNwJ.png'];
+  const imageUrls = [
+    'https://raw.githubusercontent.com/Kone457/Nexus/refs/heads/main/Ruta-(1)/A.png',
+    'https://qu.ax/fYNwJ.png'
+  ];
+
+  const randomUrl = imageUrls[Math.floor(Math.random() * imageUrls.length)];
 
   await conn.sendMessage(m.chat, {
-    image: { url: imageUrl }
+    image: { url: randomUrl }
   }, { quoted: m });
 };
 
-handler.customPrefix = /^A$/i; 
-handler.command = new RegExp(); 
+handler.customPrefix = /^A$/i;
+handler.command = new RegExp();
 handler.group = false;
 handler.admin = false;
 handler.botAdmin = false;
