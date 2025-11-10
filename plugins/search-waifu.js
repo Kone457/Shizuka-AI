4bcb30566cda80ef50bc97f75f8c2b8bfc103fbe
Carlos <supernumerovirtual@gmail.com>
2025-11-10 08:59:55 -0500

    Crear search-waifu.js

---
+++ plugins/search-waifu.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text }) => {
+  try {
+    const sender = m.sender;
+    const senderName = await conn.getName(sender);
+
+    const res = await fetch('https://api.waifu.pics/sfw/waifu');
+    const json = await res.json();
+    const imageUrl = json.url;
+
+    const caption = `💗 Aquí tienes${senderName}...\n> ¿te enamoraste? ✨`;
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: { url: imageUrl },
+        caption,
+        mentions: [sender]
+      },
+      { quoted: m }
+    );
+
+  } catch (error) {
+    console.error(error);
+    m.reply('> *Error al obtener la waifu.* Intenta nuevamente más tarde.');
+  }
+};
+
+handler.help = ['waifu'];
+handler.tags = ['buscadores'];
+handler.command = ['waifu'];
+
+export default handler;
