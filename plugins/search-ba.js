8c2ab9340c9783f436bf6841c959cc596f09790d
Carlos <supernumerovirtual@gmail.com>
2025-11-15 01:31:46 -0500

    Crear search-ba.js

---
+++ plugins/search-ba.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text }) => {
+  try {
+    const sender = m.sender;
+    const senderName = await conn.getName(sender);
+
+    
+    const imageUrl = 'https://carlos.wispbyte.app/random/ba';
+
+    const caption = `🎭 Aquí tienes ${senderName} ✨`;
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: { url: imageUrl },
+        caption,
+        mentions: [sender]
+      },
+      { quoted: m }
+    );
+
+  } catch (error) {
+    console.error(error);
+    m.reply('> *Error al obtener la imagen.* Intenta nuevamente más tarde.');
+  }
+};
+
+handler.help = ['ba'];
+handler.tags = ['buscadores'];
+handler.command = ['ba'];
+
+export default handler;
