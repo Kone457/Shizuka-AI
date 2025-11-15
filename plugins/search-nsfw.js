18f3fce4fbca7e7f0cd64a4933b7e1e203105db7
Carlos <supernumerovirtual@gmail.com>
2025-11-15 01:29:19 -0500

    Crear search-nsfw.js

---
+++ plugins/search-nsfw.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text }) => {
+  try {
+    const sender = m.sender;
+    const senderName = await conn.getName(sender);
+
+    const imageUrl = 'https://carlos.wispbyte.app/random/nsfw';
+
+    const caption = `📸 Aquí tienes ${senderName} ✨`;
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: { url: imageUrl },
+        caption,
+        mentions: [sender]
+      },
+      { quoted: m }
+    );
+
+  } catch (error) {
+    console.error(error);
+    m.reply('> *Error al obtener la imagen.* Intenta nuevamente más tarde.');
+  }
+};
+
+handler.help = ['nsfw'];
+handler.tags = ['buscadores'];
+handler.command = ['nsfw'];
+
+export default handler;
