9042a883f69df06081a36155473804af6b567a03
Carlos <supernumerovirtual@gmail.com>
2025-11-20 18:23:58 -0500

    Crear info-reportar.js

---
+++ plugins/info-reportar.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el error que desea reportar.`, m)
+    if (text.length < 10) return conn.reply(m.chat, `${emoji} Especifique bien el error, mínimo 10 caracteres.`, m)
+    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} *Máximo 1000 caracteres para enviar el error.`, m)
+
+    const teks = `*✖️ \`R E P O R T E\` ✖️*
+
+☁️ Número:
+• Wa.me/${m.sender.split`@`[0]}
+
+👤 Usuario: 
+• ${m.pushName || 'Anónimo'}
+
+💬 Mensaje:
+• ${text}`
+
+    
+    await conn.reply(
+        '5355699866@s.whatsapp.net',
+        m.quoted ? teks + m.quoted.text : teks,
+        m,
+        { mentions: conn.parseMention(teks) }
+    )
+
+    
+    m.reply(`━━━━━━━━━━━━━━━
+✖️ *R E P O R T E  E N V I A D O* ✖️
+━━━━━━━━━━━━━━━
+
+📨 Tu mensaje llegó a mi creador.  
+⚠️ Aviso: *informes falsos* → *baneo*.  
+
+🌐 Mantén la comunidad segura y justa 🌐
+━━━━━━━━━━━━━━━`)
+}
+
+handler.help = ['reportar']
+handler.tags = ['info']
+handler.command = ['reporte', 'report', 'reportar', 'bug', 'error']
+
+export default handler
