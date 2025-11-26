43da32bd189eb0ce86e329f88b7a8f04c9e97711
Carlos <supernumerovirtual@gmail.com>
2025-11-26 08:10:33 -0500

    Crear info-mcserver.js

---
+++ plugins/info-mcserver.js
@@ -0,0 +1,23 @@
+
+let handler = async (m, { conn }) => {
+  const caption = `
+🔮✨ Bienvenido a *Mistic-Craft* ✨🔮
+🌐 Conéctate y únete a la aventura
+
+📡 *IP del servidor:* \`noel.hidencloud.com\`
+🔌 *Puerto:* \`24678\`
+
+⚔️ Que la magia te acompañe ⚔️
+  `;
+
+  await conn.sendMessage(m.chat, {
+    image: { url: "https://files.catbox.moe/q459gs.jpg" }, 
+    caption
+  }, { quoted: m });
+};
+
+handler.help = ['mc-ip'];
+handler.tags = ['info'];
+handler.command = ['mc-ip'];
+
+export default handler;
