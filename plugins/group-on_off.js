bfe1363cc05907d4f68746b81f490971d711792d
Carlos <supernumerovirtual@gmail.com>
2025-11-27 15:47:28 -0500

    Actualizar group-on_off.js

--- plugins/group-on_off.js
+++ plugins/group-on_off.js
@@ -47,17 +47,8 @@ let handler = async (m, { conn, args, command }) => {
       break;
 
     case 'reaction':
-    case 'reaccion':
-      if (!m.isGroup) {
-        if (!isOwner) {
-          global.dfail('group', m, conn)
-          throw false
-        }
-      } else if (!isAdmin) {
-        global.dfail('admin', m, conn)
-        throw false
-      }
-      chat.reaction = isEnable
+      chatData.nsfw = status;
+      reply('reaction');
       break;
 
     case 'alerts':
