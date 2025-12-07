b70211c3ecacfd340495320ccf4c6517a048f407
Carlos <supernumerovirtual@gmail.com>
2025-12-06 21:21:13 -0500

    Actualizar info-reportar.js

--- plugins/info-reportar.js
+++ plugins/info-reportar.js
@@ -1,7 +1,26 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el error que desea reportar.`, m)
+    if (!text) {
+        return conn.reply(
+            m.chat,
+            `✖️ *USO DEL COMANDO ${usedPrefix + command}* ✖️
+
+📌 Este comando sirve para reportar errores o fallos del bot.
+
+✅ Ejemplo:
+${usedPrefix + command} El comando .menu no funciona correctamente
+
+⚠️ Reglas:
+• Mínimo 10 caracteres en el reporte.
+• Máximo 1000 caracteres.
+• Informes falsos pueden ocasionar *baneo*.
+
+🌐 Mantén la comunidad segura y justa 🌐`,
+            m
+        )
+    }
+
     if (text.length < 10) return conn.reply(m.chat, `${emoji} Especifique bien el error, mínimo 10 caracteres.`, m)
-    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} *Máximo 1000 caracteres para enviar el error.`, m)
+    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} *Máximo 1000 caracteres para enviar el error.*`, m)
 
     const teks = `*✖️ \`R E P O R T E\` ✖️*
 
@@ -14,7 +33,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 💬 Mensaje:
 • ${text}`
 
-    
     await conn.reply(
         '5355699866@s.whatsapp.net',
         m.quoted ? teks + m.quoted.text : teks,
@@ -22,7 +40,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         { mentions: conn.parseMention(teks) }
     )
 
-    
     m.reply(`━━━━━━━━━━━━━━━
 ✖️ *R E P O R T E  E N V I A D O* ✖️
 ━━━━━━━━━━━━━━━
