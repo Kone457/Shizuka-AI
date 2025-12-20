797d1f0ca0c570440ca3791be3905de770363613
Carlos <supernumerovirtual@gmail.com>
2025-12-20 18:35:35 -0500

    Actualizar group-on_off.js

--- plugins/group-on_off.js
+++ plugins/group-on_off.js
@@ -20,6 +20,16 @@ let handler = async (m, { conn, args, command, isOwner }) => {
       reply('Anti Enlaces');
       break;
 
+     case 'antilink':
+      if (m.isGroup) {
+        if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn)
+          throw false
+        }
+      }
+      chat.antiLink = isEnable
+      break
+
     case 'rpg':
     case 'economia':
       chatData.rpg = status;
