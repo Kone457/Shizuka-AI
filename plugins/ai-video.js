536b3b8f7736f895c073782f3a6c20ed7dba89aa
Carlos <supernumerovirtual@gmail.com>
2025-12-26 09:30:49 -0500

    Actualizar ai-video.js

--- plugins/ai-video.js
+++ plugins/ai-video.js
@@ -1,25 +1,40 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text }) => {
-  if (!text) return conn.reply(m.chat, '> Escribe un texto para generar el sticker.', m);
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) {
+    await conn.sendMessage(m.chat, { react: { text: '❔', key: m.key } });
+    return m.reply(`> Escribe el texto para generar el sticker animado.\n\n*Ejemplo:* ${usedPrefix + command} Cenix es Gey`);
+  }
 
   try {
+    await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } });
+
     const apiUrl = `https://api-faa.my.id/faa/anyabrat-vid?text=${encodeURIComponent(text)}`;
     const res = await fetch(apiUrl);
-    
-    if (!res.ok) throw new Error('Error en la API');
+
+    if (!res.ok) {
+      await conn.sendMessage(m.chat, { react: { text: '⚠️', key: m.key } });
+      return;
+    }
+
     const buffer = await res.buffer();
 
-    await conn.sendMessage(m.chat, { sticker: buffer }, { quoted: m });
+    await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
     
+    await conn.sendMessage(
+      m.chat, 
+      { sticker: buffer }, 
+      { quoted: m }
+    );
+
   } catch (error) {
     console.error(error);
-    conn.reply(m.chat, '> Ocurrió un error al procesar la solicitud.', m);
+    await conn.sendMessage(m.chat, { react: { text: '💥', key: m.key } });
   }
 };
 
-handler.help = ['anyabrat'];
+handler.help = ['brat'];
 handler.tags = ['ia'];
-handler.command = ['brat', 'anyasticker'];
+handler.command = ['brat'];
 
 export default handler;
