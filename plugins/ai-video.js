5c37b77dfc10e7febc66813e6c882a647920daa5
Carlos <supernumerovirtual@gmail.com>
2025-12-26 09:02:34 -0500

    Actualizar ai-video.js

--- plugins/ai-video.js
+++ plugins/ai-video.js
@@ -16,26 +16,21 @@ let handler = async (m, { conn, args }) => {
       { quoted: m }
     );
 
-    // Realizamos la petición a la API
     const res = await fetch(`${ANYABRAT_API_PATH}?text=${encodeURIComponent(text)}`);
 
-    // Si la respuesta no es 200 OK, mostrar un error
+    
     if (!res.ok) {
       return m.reply(`> Error al contactar con la API. Código de error: ${res.status}`);
     }
 
-    // Intentar parsear la respuesta como JSON
-    const json = await res.json();
+    
+    const videoBuffer = await res.buffer();
 
-    // Verificar si la API devuelve una URL del vídeo
-    const videoUrl = json?.result;
-
-    if (!videoUrl) {
-      return conn.reply(m.chat, '> No se pudo obtener el vídeo. La respuesta de la API no contiene un URL válido.');
+    if (!videoBuffer) {
+      return conn.reply(m.chat, '> No se pudo obtener el vídeo.');
     }
 
-    // Enviar el video a la conversación (usamos la URL directamente)
-    await conn.sendMessage(m.chat, { video: { url: videoUrl }, caption: 'Aquí tienes el vídeo generado:', edit: key });
+    await conn.sendMessage(m.chat, { video: videoBuffer, caption: 'Aquí tienes el vídeo generado:', edit: key });
   } catch (error) {
     console.error(error);
     await m.reply(`> Ocurrió un error al procesar tu solicitud: ${error.message}`);
