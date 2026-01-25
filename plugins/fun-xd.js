b1b3e8ca959aff823ad4d120729f833b60a82dfd
Carlos <supernumerovirtual@gmail.com>
2026-01-24 21:34:15 -0500

    Crear fun-xd.js

---
+++ plugins/fun-xd.js
@@ -0,0 +1,29 @@
+const handler = async (m, { conn }) => {
+  const texto = (m.text || '').trim();
+
+
+  const textoLower = texto.toLowerCase();
+
+
+  const xdRegex = /xd+/i;
+
+  if (!xdRegex.test(textoLower)) return;
+
+  const imageUrl = 'https://files.catbox.moe/990b6k.jpg';
+
+
+  await conn.sendMessage(m.chat, {
+    image: { url: imageUrl }
+  }, { quoted: m });
+};
+
+
+handler.customPrefix = /xd+/i; 
+handler.command = new RegExp(); 
+handler.group = false;
+handler.admin = false;
+handler.botAdmin = false;
+handler.tags = ['imagen'];
+handler.help = ['xd'];
+
+export default handler;
