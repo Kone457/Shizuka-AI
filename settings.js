58a5465c8dac4542fcca9a4a20e0115137ab0a54
Carlos <supernumerovirtual@gmail.com>
2026-02-07 01:38:23 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ global.botNumber = ""
 global.owner = [
   ["5355699866", "Carlos 👑", true],
   ["595975677765", "David", true],
-  ["5363870693", "Magical", true],
+  ["", "Magical", true],
   ["17054102764"],
   ["573238788888"]
 ]
