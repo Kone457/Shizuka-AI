40713d944dee48bd045d43b3a57aec22332410e9
Carlos <supernumerovirtual@gmail.com>
2026-03-09 13:36:53 -0400

    Actualizar main-owner.js

--- plugins/main-owner.js
+++ plugins/main-owner.js
@@ -1,4 +1,3 @@
-import fs from 'fs'
 
 export default {
   command: ['creator', 'owner', 'creador'],
@@ -6,21 +5,18 @@ export default {
 
   run: async (client, m) => {
 
-    const botId = client?.user?.id.split(':')[0] + '@s.whatsapp.net' || ''
-    const botSettings = global.db.data.settings[botId] || {}
-
-    const owner = botSettings.owner || '+5355699866@s.whatsapp.net'
-    const botname = botSettings.namebot || 'Bot'
-    const banner = botSettings.banner || ''
+    const owner = '5355699866@s.whatsapp.net'
+    const botname = 'รђเzยкค-คเ'
+    const banner = ''
 
     const ownerNumber = owner.split('@')[0]
 
     const text = `
-╭━〔 ˚୨•(=^●ω●^=)• ⊹ 𝑪𝑹𝑬𝑨𝑫𝑶𝑹 ⊹ •(=^●ω●^=)•୨˚ 〕━╮
+╭━〔 ˚୨•(=^●ω●^=)• ⊹ 𝑪𝑹𝑬𝑨𝑫𝑶𝑹 ⊹ 〕━╮
 
 » ˚୨•(=^●ω●^=)• ⊹ Información ⊹
 
-➤ Creador: @${ownerNumber}
+➤ Creador: @Oculto por privacidad
 ➤ Bot: ${botname}
 ➤ Contacto disponible abajo
 
@@ -31,50 +27,25 @@ export default {
 ╰━━━━━━━━━━━━━━━━━━━━╯
 `.trim()
 
-    const contact = {
-      key: {
-        participants: "0@s.whatsapp.net",
-        remoteJid: "status@broadcast",
-        fromMe: false,
-        id: "Creator"
-      },
-      message: {
-        contactMessage: {
-          displayName: botname,
-          vcard: `BEGIN:VCARD
+    const vcard = `
+BEGIN:VCARD
 VERSION:3.0
-N:;Owner;;;
-FN:Owner
+N:;Creador;;;
+FN:Creador
 TEL;type=CELL;type=VOICE;waid=${ownerNumber}:${ownerNumber}
-END:VCARD`
+END:VCARD
+`.trim()
+
+    await client.sendMessage(
+      m.chat,
+      {
+        text: text,
+        contacts: {
+          displayName: 'Creador',
+          contacts: [{ vcard }]
         }
-      }
-    }
-
-    if (banner) {
-
-      await client.sendMessage(
-        m.chat,
-        {
-          image: { url: banner },
-          caption: text,
-          mentions: [owner]
-        },
-        { quoted: contact }
-      )
-
-    } else {
-
-      await client.sendMessage(
-        m.chat,
-        {
-          text: text,
-          mentions: [owner]
-        },
-        { quoted: contact }
-      )
-
-    }
-
+      },
+      { quoted: m }
+    )
   }
 }
