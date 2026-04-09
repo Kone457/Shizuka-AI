0b5b6b6eb790dfe1326bc9259f66bf00ad23ece0
Carlos <supernumerovirtual@gmail.com>
2026-04-09 12:50:39 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-global.owner = ['5355699866', '5363870693', '17054102764']
+global.owner = ['5355699866', '5363870693', '17054102764', '595987301197']
 
 global.sessionName = 'Sessions/Owner'
 global.version = '4.7'
