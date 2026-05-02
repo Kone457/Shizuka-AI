b4206ca4888a4c2c71d311f3d2e54104c3dffbeb
Carlos <supernumerovirtual@gmail.com>
2026-05-02 09:30:59 -0400

    Crear nsfw-fuck.js

---
+++ plugins/nsfw-fuck.js
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let who;
+    if (m.mentionedJid.length > 0) {
+        who = m.mentionedJid[0];
+    } else if (m.quoted) {
+        who = m.quoted.sender;
+    } else {
+        who = m.sender;
+    }
+
+    let name = conn.getName(who);
+    let name2 = conn.getName(m.sender);
+    m.react('🥵');
+
+    let str;
+    if (m.mentionedJid.length > 0) {
+        str = `\`${name2}\` *se lo metió sabrosamente a* \`${name || who}\`.`;
+    } else if (m.quoted) {
+        str = `\`${name2}\` *cogió fuertemente a* \`${name || who}\`.`;
+    } else {
+        str = `\`${name2}\` *está cogiendo! >.<*`.trim();
+    }
+    
+    if (m.isGroup) {
+        let pp = 'https://magical.vercel.app/files/1b70ddc50331.mp4'; 
+        let pp2 = ''; 
+        let pp3 = '';
+        let pp4 = '';
+        
+        const videos = [pp, pp2, pp3, pp4];
+        const video = videos[Math.floor(Math.random() * videos.length)];
+
+        let mentions = [who];
+        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, mentions }, { quoted: m });
+    }
+}
+
+handler.help = ['fuck'];
+handler.tags = ['nsfw'];
+handler.command = ['fuck'];
+handler.group = true;
+handler.nsfw = true;
+
+export default handler;
