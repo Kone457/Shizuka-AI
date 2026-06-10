a6ba3640cf4f5de6b2e3defc7e8e372e2b3f7c2c
Carlos <supernumerovirtual@gmail.com>
2026-06-10 08:52:38 -0400

    Crear group-warnings.js

---
+++ plugins/group-warnings.js
@@ -0,0 +1,22 @@
+let handler = async (m, { conn }) => {
+    let user = m.mentionedJid?.[0] || m.quoted?.sender || m.sender
+
+    let users = global.db.data.users
+    if (!users[user]) users[user] = {}
+
+    users[user].warn = users[user].warn || 0
+
+    conn.reply(
+        m.chat,
+        `📋 Advertencias de @${user.split('@')[0]}\n\n✦ Total: *${users[user].warn}*`,
+        m,
+        { mentions: [user] }
+    )
+}
+
+handler.help = ['warnings']
+handler.tags = ['grupo']
+handler.command = ['warnings']
+handler.group = true
+
+export default handler
