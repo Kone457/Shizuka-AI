aee259e8be4a8a1ea502802ff5eeb1431b94e409
Carlos <supernumerovirtual@gmail.com>
2026-07-04 14:27:05 -0400

    Actualizar nsfw-reaction.js

--- plugins/nsfw-reaction.js
+++ plugins/nsfw-reaction.js
@@ -2,14 +2,19 @@ import fs from 'fs'
 import path from 'path'
 
 let handler = async (m, { conn, command }) => {
-    const mentioned = m.mentionedJid && m.mentionedJid.length > 0
-    let who = mentioned ? m.mentionedJid[0] : (m.quoted ? m.quoted.sender : m.sender)
-    let name = conn.getName(who)
-    let name2 = conn.getName(m.sender)
+    let mentioned = await m.mentionedJid
+    let who = mentioned.length > 0
+        ? mentioned[0]
+        : (m.quoted ? m.quoted.sender : m.sender)
+
+    let isMention = mentioned.length > 0
+
+    let name = await conn.getName(who)
+    let name2 = await conn.getName(m.sender)
 
     let jsonPath = path.join(process.cwd(), 'lib', 'reaction.json')
-    let reactions;
-    
+    let reactions
+
     try {
         reactions = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'))
     } catch (e) {
@@ -21,31 +26,52 @@ let handler = async (m, { conn, command }) => {
     let videos = reactions[cmd]
     let texts = reactions[`text-${cmd}`]
 
-    if (!videos || !texts) return m.reply(`❌ No se encontraron datos para la reacción NSFW: ${cmd}`)
+    if (!videos || !texts) {
+        return m.reply(`❌ No se encontraron datos para la reacción NSFW: ${cmd}`)
+    }
 
-    let str = mentioned || m.quoted 
-        ? `\`${name2}\` ${texts[0]} \`${name || who}\`` 
+    let str = (isMention || m.quoted)
+        ? `\`${name2}\` ${texts[0]} \`${name || who}\``
         : `\`${name2}\` ${texts[1]}`
 
     let video = videos[Math.floor(Math.random() * videos.length)]
-    conn.sendMessage(m.chat, { 
-        video: { url: video }, 
-        gifPlayback: true, 
-        caption: str, 
-        mentions: [who, m.sender] 
-    }, { quoted: m })
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            video: { url: video },
+            gifPlayback: true,
+            caption: str,
+            mentions: (isMention || m.quoted)
+                ? [who, m.sender]
+                : [m.sender]
+        },
+        { quoted: m }
+    )
 }
 
 const nsfwCommands = [
-    'preg', '69', 'anal', 'blowjob', 'boobjob', 'cum', 'fap', 
-    'footjob', 'fuck', 'grabboobs', 'lickpussy', 'spank', 'suckboobs', 
-    'undress', 'yuri'
+    'preg',
+    '69',
+    'anal',
+    'blowjob',
+    'boobjob',
+    'cum',
+    'fap',
+    'footjob',
+    'fuck',
+    'grabboobs',
+    'lickpussy',
+    'spank',
+    'suckboobs',
+    'undress',
+    'yuri'
 ]
 
 handler.help = nsfwCommands
 handler.tags = ['nsfw']
 handler.command = new RegExp(`^(${nsfwCommands.join('|')})$`, 'i')
 handler.group = true
-handler.nsfw = true 
+handler.nsfw = true
 
 export default handler
