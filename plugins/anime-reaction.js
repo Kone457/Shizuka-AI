7ead79a3915cd04ded5540ed0969c64da5649716
Carlos <supernumerovirtual@gmail.com>
2026-07-04 14:27:35 -0400

    Actualizar anime-reaction.js

--- plugins/anime-reaction.js
+++ plugins/anime-reaction.js
@@ -2,14 +2,19 @@ import fs from 'fs'
 import path from 'path'
 
 let handler = async (m, { conn, command }) => {
-    const mentioned = m.mentionedJid && m.mentionedJid.length > 0
-    let who = mentioned ? m.mentionedJid[0] : (m.quoted ? m.quoted.sender : m.sender)
-    let name = conn.getName(who)
-    let name2 = conn.getName(m.sender)
+    let mentioned = await m.mentionedJid
+    let who = mentioned.length > 0
+        ? mentioned[0]
+        : (m.quoted ? m.quoted.sender : m.sender)
+
+    let isMention = mentioned.length > 0
+
+    let name = await conn.getName(who)
+    let name2 = await conn.getName(m.sender)
 
     let jsonPath = path.join(process.cwd(), 'lib', 'reaction.json')
-    let reactions;
-    
+    let reactions
+
     try {
         reactions = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'))
     } catch (e) {
@@ -21,26 +26,65 @@ let handler = async (m, { conn, command }) => {
     let videos = reactions[cmd]
     let texts = reactions[`text-${cmd}`]
 
-    if (!videos || !texts) return m.reply(`❌ No se encontraron datos para la reacción: ${cmd}`)
+    if (!videos || !texts) {
+        return m.reply(`❌ No se encontraron datos para la reacción: ${cmd}`)
+    }
 
-    let str = mentioned || m.quoted 
-        ? `\`${name2}\` ${texts[0]} \`${name || who}\`` 
+    let str = (isMention || m.quoted)
+        ? `\`${name2}\` ${texts[0]} \`${name || who}\``
         : `\`${name2}\` ${texts[1]}`
 
     let video = videos[Math.floor(Math.random() * videos.length)]
-    conn.sendMessage(m.chat, { 
-        video: { url: video }, 
-        gifPlayback: true, 
-        caption: str, 
-        mentions: [who, m.sender] 
-    }, { quoted: m })
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            video: { url: video },
+            gifPlayback: true,
+            caption: str,
+            mentions: (isMention || m.quoted)
+                ? [who, m.sender]
+                : [m.sender]
+        },
+        { quoted: m }
+    )
 }
 
 const sfwCommands = [
-    'angry', 'bath', 'bite', 'bleh', 'blush', 'bored', 'clap', 'coffee', 'cry', 
-    'cuddle', 'dance', 'drunk', 'eat', 'facepalm', 'happy', 'hug', 'kill', 'kiss', 
-    'laugh', 'lick', 'love', 'pat', 'poke', 'pout', 'punch', 'run', 'sad', 'scared', 
-    'seduce', 'shy', 'slap', 'sleep', 'smoke', 'think'
+    'angry',
+    'bath',
+    'bite',
+    'bleh',
+    'blush',
+    'bored',
+    'clap',
+    'coffee',
+    'cry',
+    'cuddle',
+    'dance',
+    'drunk',
+    'eat',
+    'facepalm',
+    'happy',
+    'hug',
+    'kill',
+    'kiss',
+    'laugh',
+    'lick',
+    'love',
+    'pat',
+    'poke',
+    'pout',
+    'punch',
+    'run',
+    'sad',
+    'scared',
+    'seduce',
+    'shy',
+    'slap',
+    'sleep',
+    'smoke',
+    'think'
 ]
 
 handler.help = sfwCommands
