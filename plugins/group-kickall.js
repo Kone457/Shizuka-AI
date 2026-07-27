1a1095fbc8b39c57c5ef32fb1e1c01d0f5ff480e
Carlos <supernumerovirtual@gmail.com>
2026-07-27 18:07:11 -0400

    Actualizar group-kickall.js

--- plugins/group-kickall.js
+++ plugins/group-kickall.js
@@ -2,22 +2,28 @@ var handler = async (m, { conn, participants }) => {
   const groupInfo = await conn.groupMetadata(m.chat);
   const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
   const ownerBot = globalThis.owner[0][0] + '@s.whatsapp.net';
-  const protectedOwners = global.owner.map(o => o[0] + '@s.whatsapp.net');
+
 
   let targets = participants
     .map(p => p.id)
-    .filter(id => id !== conn.user.jid)
-    .filter(id => id !== ownerGroup)
-    .filter(id => id !== ownerBot)
-    .filter(id => !protectedOwners.includes(id));
+    .filter(id => id !== conn.user.jid)          
+    .filter(id => id !== ownerGroup)             
+    .filter(id => id !== ownerBot);              
 
   if (!targets.length) {
-    return conn.reply(m.chat, `✧ No hay usuarios válidos para expulsar.`, m);
+    return conn.reply(m.chat, 
+      `✧ No hay usuarios válidos para expulsar.`, 
+      m
+    );
   }
+
 
   await conn.groupParticipantsUpdate(m.chat, targets, 'remove');
 
-  await conn.reply(m.chat, `✦ Todos los usuarios han sido expulsados del grupo...`, m);
+  await conn.reply(m.chat, 
+    `✦ Todos los usuarios han sido expulsados del grupo...`, 
+    m
+  );
 };
 
 handler.help = ['kickall'];
