624a7002c72e62e2f826555a9e57a9677eda04b1
Carlos <supernumerovirtual@gmail.com>
2026-08-28 17:34:32 -0400

    Actualizar _level.js

--- plugins/_level.js
+++ plugins/_level.js
@@ -2,18 +2,50 @@ import { canLevelUp, xpRange } from '../lib/levelling.js';
 
 let handler = m => m;
 handler.before = async function (m, { conn }) {
-    let user = globalThis.db.data.chats[m.chat]
+    if (!m.isGroup) return true;
+    
+    let chat = globalThis.db.data.chats[m.chat] ||= {};
+    if (chat.level === false) return true;
+
+    let user = globalThis.db.data.users[m.sender];
+    if (!user) return true;
+
+    user.level = user.level || 0;
+    user.exp = user.exp || 0;
 
     let before = user.level;
 
-    while (canLevelUp(user.level, user.exp, globalThis.multiplier)) {
+    while (canLevelUp(user.level, user.exp, globalThis.multiplier || 1)) {
         user.level++;
     }
+
     if (before !== user.level) {
-        let { min, max } = xpRange(user.level, globalThis.multiplier);
+        let { min, xp, max } = xpRange(user.level, globalThis.multiplier || 1);
+        let alertImage = 'https://files.evogb.win/ChAkmb.jpg';
+
+        let txt = `
+╭─ׅ─ׅ┈ ─๋︩︪─❖─๋︩︪─┈─ׅ─ׅ╮
+╭╼✨ 𝐍𝐔𝐄𝐕𝐎 𝐍𝐈𝐕𝐄𝐋 𝐀𝐋𝐂𝐀𝐍𝐙𝐀𝐃𝐎 ✨╮
+┃֪࣪
+├ׁ̟̇❍✎ 👤 *Usuario:* @${m.sender.split('@')[0]}
+├ׁ̟̇❍✎ 📈 *Rango:* \`${before}\` ➔ \`${user.level}\`
+├ׁ̟̇❍✎ ⚡ *Experiencia:* \`${user.exp} / ${max}\`
+╰─ׅ─ׅ┈ ─๋︩︪─❖─๋︩︪─┈─ׅ─ׅ╯
+
+> 🌟 *¡Felicidades! Sigue interactuando para desbloquear más sorpresas
+        `.trim();
+
+        await conn.sendMessage(m.chat, {
+            image: { url: alertImage },
+            caption: txt,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                isForwarded: true
+            }
+        }, { quoted: m });
     }
 
-     return true;
+    return true;
 };
 
 export default handler;
