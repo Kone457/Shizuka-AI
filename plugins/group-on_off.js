32e37d52816ae2aa771652e4c926131b4221421e
Carlos <supernumerovirtual@gmail.com>
2026-08-28 19:42:25 -0400

    Actualizar group-on_off.js

--- plugins/group-on_off.js
+++ plugins/group-on_off.js
@@ -4,24 +4,61 @@ let handler = async (m, { conn, args, command, isOwner }) => {
 
   const setting = args[0]?.toLowerCase();
 
-  const chatData = globalThis.db.data.chats[m.chat] ||= {};
+  globalThis.db.data.chats ||= {};
+  globalThis.db.data.settings ||= {};
+
+  const chatData =
+    globalThis.db.data.chats[m.chat] ||= {};
+
+  const botJid = conn.user.jid;
+
   const botSettings =
-    globalThis.db.data.settings?.[conn.user.jid] ||= {};
+    globalThis.db.data.settings[botJid] ||= {};
 
-  if (!('welcome' in chatData)) chatData.welcome = true;
-  if (!('antiLink' in chatData)) chatData.antiLink = true;
-  if (!('antilinks' in chatData)) chatData.antilinks = true;
-  if (!('antiprivado' in chatData)) chatData.antiprivado = true;
-  if (!('economy' in chatData)) chatData.economy = true;
-  if (!('gacha' in chatData)) chatData.gacha = true;
-  if (!('level' in chatData)) chatData.level = true;
-  if (!('adminonly' in chatData)) chatData.adminonly = false;
-  if (!('reaction' in chatData)) chatData.reaction = true;
-  if (!('nsfw' in chatData)) chatData.nsfw = false;
-  if (!('alerts' in chatData)) chatData.alerts = true;
-  if (!('notprefix' in chatData)) chatData.notprefix = false;
+  if (!('welcome' in chatData))
+    chatData.welcome = true;
 
-  const statusIcon = conf => conf ? '🟢' : '🔴';
+  if (!('antiLink' in chatData))
+    chatData.antiLink = true;
+
+  if (!('antilinks' in chatData))
+    chatData.antilinks = true;
+
+  if (!('antiprivado' in chatData))
+    chatData.antiprivado = true;
+
+  if (!('economy' in chatData))
+    chatData.economy = true;
+
+  if (!('rpg' in chatData))
+    chatData.rpg = true;
+
+  if (!('gacha' in chatData))
+    chatData.gacha = true;
+
+  if (!('level' in chatData))
+    chatData.level = true;
+
+  if (!('adminonly' in chatData))
+    chatData.adminonly = false;
+
+  if (!('reaction' in chatData))
+    chatData.reaction = true;
+
+  if (!('nsfw' in chatData))
+    chatData.nsfw = false;
+
+  if (!('alerts' in chatData))
+    chatData.alerts = true;
+
+  if (!('notprefix' in chatData))
+    chatData.notprefix = false;
+
+  if (!('jadibotmd' in botSettings))
+    botSettings.jadibotmd = false;
+
+  const statusIcon = conf =>
+    conf ? '🟢' : '🔴';
 
   const configList = `
 ╭─ׅ─ׅ┈ ─๋︩︪─❖─๋︩︪─┈─ׅ─ׅ╮
@@ -38,7 +75,7 @@ let handler = async (m, { conn, args, command, isOwner }) => {
 ├ׁ̟̇❍✎ nsfw ${statusIcon(chatData.nsfw)}
 ├ׁ̟̇❍✎ alertas ${statusIcon(chatData.alerts)}
 ├ׁ̟̇❍✎ notprefix ${statusIcon(chatData.notprefix)}
-├ׁ̟̇❍✎ serbot ${statusIcon(botSettings?.jadibotmd)}
+├ׁ̟̇❍✎ serbot ${statusIcon(botSettings.jadibotmd)}
 ╰─ׅ─ׅ┈ ─๋︩︪─❖─๋︩︪─┈─ׅ─ׅ╯
 
 ✎ Ejemplo:
@@ -49,15 +86,23 @@ ${command} level
     return m.reply(configList);
   }
 
-  const status = command.toLowerCase() === 'on';
+  const status =
+    command.toLowerCase() === 'on';
 
-  const reply = async (name, customMsg = null, image = null) => {
-    const textMsg = customMsg || `
+  const reply = async (
+    name,
+    customMsg = null,
+    image = null
+  ) => {
+
+    const textMsg =
+      customMsg ||
+      `
 ╭─ׅ─ׅ┈ ─๋︩︪─❖─๋︩︪─┈─ׅ─ׅ╮
 ╭╼⚙️ 𝐅𝐔𝐍𝐂𝐈Ó𝐍 ⚙️╮
 ┃֪࣪
 ├ׁ̟̇❍✎ ${name}
-├ׁׁ̟̇❍✎ ${status ? '🟢 ACTIVADA' : '🔴 DESACTIVADA'}
+├ׁ̟̇❍✎ ${status ? '🟢 ACTIVADA' : '🔴 DESACTIVADA'}
 ╰─ׅ─ׅ┈ ─๋︩︪─❖─๋︩︪─┈─ׅ─ׅ╯
 `.trim();
 
@@ -83,74 +128,107 @@ ${command} level
     case 'antilink':
     case 'antilinks':
     case 'antienlaces':
+
       chatData.antiLink = status;
       chatData.antilinks = status;
+
       await reply('Anti Enlaces');
       break;
+
 
     case 'antiprivado':
     case 'antiprivados':
     case 'private':
     case 'privado':
+
       chatData.antiprivado = status;
+
       await reply('Anti Privado');
       break;
 
+
     case 'rpg':
     case 'economia':
+
       chatData.rpg = status;
       chatData.economy = status;
+
       await reply('Economía');
       break;
 
+
     case 'gacha':
+
       chatData.gacha = status;
+
       await reply('Gacha');
       break;
+
 
     case 'level':
     case 'niveles':
     case 'nivel':
+
       chatData.level = status;
+
       await reply('Niveles');
       break;
+
 
     case 'modoadmin':
     case 'adminonly':
     case 'onlyadmin':
+
       chatData.adminonly = status;
+
       await reply('Modo Admin');
       break;
 
+
     case 'nsfw':
+
       chatData.nsfw = status;
+
       await reply('NSFW');
       break;
 
+
     case 'bienvenida':
     case 'welcome':
+
       chatData.welcome = status;
+
       await reply('Bienvenida');
       break;
 
+
     case 'reaccion':
     case 'reaction':
+
       chatData.reaction = status;
+
       await reply('Reacciones');
       break;
 
+
     case 'alerts':
     case 'alertas':
+
       chatData.alerts = status;
+
       await reply('Alertas');
       break;
+
 
     case 'notprefix':
     case 'noprefix':
     case 'sinprefijo':
+
       chatData.notprefix = status;
+
       await reply('Sin Prefijo (NotPrefix)');
       break;
+
 
     case 'serbot':
     case 'jadibot':
@@ -172,7 +250,9 @@ ${command} level
       await reply('Subbots (JadiBot)');
       break;
 
+
     default:
+
       return m.reply(`
 ╭─ׅ─ׅ┈ ─๋︩︪─❖─๋︩︪─┈─ׅ─ׅ╮
 ╭╼⚙️ 𝐎𝐏𝐂𝐈Ó𝐍 𝐍𝐎 𝐕Á𝐋𝐈𝐃𝐀 ⚙️╮
@@ -185,9 +265,18 @@ ${configList}
   }
 };
 
-handler.help = ['on', 'off'];
+handler.help = [
+  'on',
+  'off'
+];
+
 handler.tags = ['grupo'];
-handler.command = ['on', 'off'];
+
+handler.command = [
+  'on',
+  'off'
+];
+
 handler.admin = true;
 handler.botAdmin = false;
 
